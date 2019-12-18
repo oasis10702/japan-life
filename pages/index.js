bc85cd535881d82c0eef783fa6fdb02547a1e116
oasis10702 <oasis10702@gmail.com>
2019-12-18 23:21:49 +0900

    add material-ui

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,27 @@ import ReactFullpage from '@fullpage/react-fullpage';
 const Home = () => (
   <ReactFullpage
     scrollingSpeed={1000}
-    render={({ fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <Head>
-            <title>Home</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-          <div className="section">
-            <p>Section 3</p>
-          </div>
-          <div className="section">
-            <p>Section 4</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
+    render={({ fullpageApi }) => (
+      <ReactFullpage.Wrapper>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="section">
+          <p>Section 1 (welcome to fullpage.js)</p>
+          <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+        </div>
+        <div className="section">
+          <p>Section 2</p>
+        </div>
+        <div className="section">
+          <p>Section 3</p>
+        </div>
+        <div className="section">
+          <p>Section 4</p>
+        </div>
+      </ReactFullpage.Wrapper>
+    )}
   />
 );
 
