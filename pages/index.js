846ee26ce0869c044b49065e40c1fd03b16b9b7d
oasis10702 <oasis10702@gmail.com>
2019-12-18 22:42:52 +0900

    add eslint

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,28 @@
-import React from "react";
-import Head from "next/head";
-import ReactFullpage from "@fullpage/react-fullpage";
+import React from 'react';
+import Head from 'next/head';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 const Home = () => (
   <ReactFullpage
     scrollingSpeed={1000}
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <Head>
-            <title>Home</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-          <div className="section">
-            <p>Section 3</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
+    render={({ fullpageApi }) => (
+      <ReactFullpage.Wrapper>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="section">
+          <p>Section 1 (welcome to fullpage.js)</p>
+          <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+        </div>
+        <div className="section">
+          <p>Section 2</p>
+        </div>
+        <div className="section">
+          <p>Section 3</p>
+        </div>
+      </ReactFullpage.Wrapper>
+    )}
   />
 );
 
