6255e834dae56a4a5196ab24fe241d6cb284628c
oasis10702 <oasis10702@gmail.com>
2020-01-30 23:02:55 +0900

    refactor hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -2,37 +2,19 @@ import React from 'react';
 
 import springImg from '../../assets/images/spring.jpg';
 import autumnImg from '../../assets/images/autumn.jpg';
-import summer from '../../assets/images/summer.jpg';
-import winter from '../../assets/images/winter.jpg';
+import summerImg from '../../assets/images/summer.jpg';
+import winterImg from '../../assets/images/winter.jpg';
 import './styles.scss';
 
-const Hero = () => (
-  <>
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url(${springImg})`
-      }}
-    />
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url(${summer})`
-      }}
-    />
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url(${autumnImg})`
-      }}
-    />
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url(${winter})`
-      }}
-    />
-  </>
+const renderImage = img => (
+  <div
+    className="hero"
+    style={{
+      backgroundImage: `url(${img})`
+    }}
+  />
 );
+
+const Hero = () => [springImg, summerImg, autumnImg, winterImg].map(img => renderImage(img));
 
 export default Hero;
